563a82e18338d2ba257b74692594394fce03ed83
Sanskar gupta <127644837+Sanskargupta0@users.noreply.github.com>
2023-10-13 13:28:16 +0530

    font fix in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -80,7 +80,7 @@ const Navbar = () => {
               {navLinks.map((nav) => (
                 <li
                   key={nav.id}
-                  className={`font-pins font-medium cursor-pointer text-[16px] ${
+                  className={`font-Montserrat font-medium cursor-pointer text-[16px] ${
                     active === nav.title ? "text-white" : "text-secondary"
                   }`}
                   onClick={() => {
